18c29b1e711477cfc115bedc06a06694ba7381b2
EDUARDO PINA <eduardopina@MacBook-Air-de-EDUARDO.local>
2021-08-25 16:49:06 -0300

    options caontainers generation working fine

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer (props){
     return(
-        <div class="bottom-box">
-        <button class="botao-final" id='BF' onclick="bonus()">
-            <p class = "nao-selecionado" id="NS">Selecione os {props.numberOfItems} itens
+        <div className = "bottom-box">
+        <button className = "botao-final" id='BF'>
+            <p className = "nao-selecionado" id="NS">Selecione os {props.numberOfItems} itens
                 para fechar o pedido</p>
-            <p  class = "selecionado" id="S">Finalizar pedido</p>
+            <p  className = "selecionado" id="S">Finalizar pedido</p>
         </button>
     </div>
     )
--- src/OptionsContainer.js
+++ src/OptionsContainer.js
@@ -1,51 +1,11 @@
-let options = {
-    foodSlogan: "Primeiro, seu prato",
-    food: [{
-        name: "Burgão gostoso",
-        description: "Gostoso d+, confia na call",
-        img: "./images/burgao gostoso.jpeg",
-        price: "R$ 12,00"
-    },
-    {
-        name: "Burgão gostoso",
-        description: "Gostoso d+, confia na call",
-        img: "./images/burgao gostoso.jpeg",
-        price: "R$ 12,00"
-    },{
-        name: "Burgão gostoso",
-        description: "Gostoso d+, confia na call",
-        img: "./images/burgao gostoso.jpeg",
-        price: "R$ 12,00"
-    },{
-        name: "Burgão gostoso",
-        description: "Gostoso d+, confia na call",
-        img: "./images/burgao gostoso.jpeg",
-        price: "R$ 12,00"
-    },{
-        name: "Burgão gostoso",
-        description: "Gostoso d+, confia na call",
-        img: "./images/burgao gostoso.jpeg",
-        price: "R$ 12,00"
-    },{
-        name: "Burgão gostoso",
-        description: "Gostoso d+, confia na call",
-        img: "./images/burgao gostoso.jpeg",
-        price: "R$ 12,00"
-    },]
-}
+import Option from "./Option"
 
 export default function OptionsContainer () {
     return(
         <div className = "bloco-opcoes">
-            <p class="comando-bloco">{options.foodSlogan}</p>
-            {options.food.map(element =>(
-                <button class="opcao" id= "prato1" onclick="selecionaPrato('prato1', 'certinhoP1')" >
-                    <img src={element.img}/>
-                    <p class={element.name}>Burgão gostoso</p>
-                    <p class={element.description}>Gostoso d+, confia na call</p>
-                    <p class={element.price}>R$ 12,00</p>
-                    <ion-icon name="checkmark-circle" class="certinho" id="certinhoP1"></ion-icon>
-                </button>))}
+            <Option category = "food" />
+            <Option category = "drinks" />
+            <Option category = "dessert" />
         </div>
     )
 }
