9fb61ee137b0815343750a8668e4c04c1673a7a9
EDUARDO PINA <eduardopina@unknown.home>
2021-08-30 15:03:14 -0300

    wrong

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -41,22 +41,25 @@ const Footer = ({numberOfItems}) => {
     setInterval(verifyOrder, 10)
 
     const takeTheOrder = () => {
-       iterate(meals);
-       iterate(drinks);
-       iterate(desserts)
-
-        message = 
-`Olá, gostaria de fazer um pedido:
-    - ${food}
-    - ${beverage}
-    - ${candys}
-Total: R$ ${totalPrice.toFixed(2).toString().replace(".", ",")}`
-
-        //the message content isn't idented 'cause of its whatsApp format, wich needs to have no spacing between the line breaks and their texts
-
-        link = `https://wa.me/5521967431453?text=` + encodeURIComponent(message)
-        
-        window.open(link, "_blank")
+        if(drinks.isSelected && desserts.isSelected && meals.isSelected){
+            iterate(meals);
+            iterate(drinks);
+            iterate(desserts)
+     
+             message = 
+     `Olá, gostaria de fazer um pedido:
+         - ${food}
+         - ${beverage}
+         - ${candys}
+     Total: R$ ${totalPrice.toFixed(2).toString().replace(".", ",")}`
+     
+             //the message content isn't idented 'cause of its whatsApp format, wich needs to have no spacing between the line breaks and their texts
+     
+             link = `https://wa.me/5521967431453?text=` + encodeURIComponent(message)
+             
+             window.open(link, "_blank")
+     
+        }
     }
 
     return(
